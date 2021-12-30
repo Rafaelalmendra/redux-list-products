6bd803d00554381fae27171b4b5d62df9406cd03
Rafael Almendra <rafaelalmendra28@gmail.com>
2021-12-30 01:11:31 -0300

    random commit

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -39,7 +39,7 @@ export const Details = () => {
       ) : (
         <div className="ui placeholder segment">
           <div className="ui two column stackable center aligned grid">
-            <div className="ui vertical divider">AND</div>
+            <div className="ui vertical divider">ANDD</div>
             <div className="middle aligned row">
               <div className="column lp">
                 <img className="ui fluid image" src={image} alt={title}/>
@@ -47,7 +47,7 @@ export const Details = () => {
               <div className="column rp">
                 <h1>{title}</h1>
                 <h2>
-                  <p className="ui teal tag label">${price}</p>
+                  <p className="ui teal tag label">$ {price}</p>
                 </h2>
                 <h3 className="ui brown block header">{category}</h3>
                 <p>{description}</p>
